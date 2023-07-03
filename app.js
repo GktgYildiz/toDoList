7d44d9a5317d622acf3f5656cb766732880b2a3e
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-07-02 19:40:50 -0700

    version v1 to do list

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+// const dayNames =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var items = ["Buy Food","Cook Food","Eat Food"];
+
 app.get('/', (req, res) =>{
 
     var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0){
-        res.send("Yay it's the weekend!");
-    } else{
-        res.send("Boo! I have to work!");
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
     }
+    var day = today.toLocaleDateString("en-US", options);
+
+    // var nameOfDay =dayNames[currentDay];
+        res.render("list", {kindOfDay: day, newListItems: items});
 
 });
-
+``
+app.post('/', (req, res) =>{
+     var item = req.body.newItem;
+     items.push(item);
+    res.redirect("/");
+});
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 })
