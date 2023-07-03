62434584f439d8bc00955257c1448b6c47be270f
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-07-02 17:30:41 -0700

    initialized files

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.get('/', (req, res) =>{
+
+    var today = new Date();
+    if (today.getDay() === 6 || today.getDay() === 0){
+        res.send("Yay it's the weekend!");
+    } else{
+        res.send("Boo! I have to work!");
+    }
+
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
